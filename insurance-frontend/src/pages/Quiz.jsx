1e7d2cd565e20fd340c4cddf1072f608a4b43a3c
revyrob <kayle.robson@gmail.com>
2023-03-06 20:16:48 -0800

    added styling.  tried to add useNavigate but it screws up the sessionStorage for the num variable for the numbers in the quiz

--- insurance-frontend/src/pages/Quiz.jsx
+++ insurance-frontend/src/pages/Quiz.jsx
@@ -4,16 +4,12 @@ import { v4 as uuidv4 } from "uuid";
 import Final from '../pages/Final';
 import { Line } from "rc-progress";
 import axios from "axios";
-//import Modal from "react-bootstrap/Modal";
-//import Button from "react-bootstrap/Button";
 
 export default function Quiz() {
-  //to show modal box when answer is wrong and with the correct answer
-  //const [show, setShow] = useState(false);
-  //num of questions come from homepage session storage when user entered in a number
-  //if they did not enter in a number it defaults to 20
+  //num from session storage, entered by user on the homepage
   let num = sessionStorage.getItem("num");
-
+  // let num = parseInt(sessionNum);
+  // console.log(sessionStorage.getItem("num"))
   //use sessions storage for level
   const [questions, setQuestions] = useState("");
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -24,21 +20,19 @@ export default function Quiz() {
   const [answerCorrect, setAnswerCorrect] = useState(false);
 
   const REACT_APP_API_SERVER_URL = process.env.REACT_APP_API_SERVER_URL;
-  
-  //functions for opening and closing Modal
-  //const handleClose = () => setShow(false);
-  //const handleShow = () => setShow(true);
 
   //get data from the backend
-  const getData = () => {
+  const getQuestions = () => {
     axios
     .get(`${REACT_APP_API_SERVER_URL}questions`)
     .then((response) => {
       let data = response.data;
-      setQuestions(getRandom(data, num))
+      let randomData = getRandom(data, num);
+      setQuestions(randomData)
     })
     .catch((err) => console.log(err));
   };
+  
   
   //function that can take in an array and the number of questions required
   //gives a random objects from the array back
@@ -60,14 +54,12 @@ export default function Quiz() {
   
   //run a useEffect with the getQuestions function
   useEffect(() => {
-    getData();
+    getQuestions();
     
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   
   const handleAnswerOptionClick = (isCorrect, points) => {
-    //let answerTimer;
-    //let questionTimer;
     setAnswerSelected(true);
     if (isCorrect && (answerSelected === isCorrect)) {
       setScore(score + points);
@@ -79,13 +71,9 @@ export default function Quiz() {
     
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
-      // if (!questionTimer) {
-      setTimeout(() => {
+      // setTimeout(() => {
           setCurrentQuestion(nextQuestion);
-        }, 1000);
-      // } else {
-      //   clearTimeout(questionTimer);
-      // }
+        // }, 1000);
     } else {
       setTimeout(() => setShowScore(true), 300);
     }
@@ -115,7 +103,7 @@ export default function Quiz() {
           />
           )
           ) : (
-            <div className="pt-8 px-2">
+            <div className="pt-8 px-2 ">
             <div className="">
             <Line
             percent={((currentQuestion + 1) / questions.length) * 100}
@@ -123,13 +111,13 @@ export default function Quiz() {
             strokeColor="#379683"
             />
             </div>
-            <div className="px-2 pt-6 flex flex-col flex-wrap w-full">
-            <div className="font-bold">
+            <div className="mx-auto my-4 px-4 py-8 h-full w-full md:w-3/4 flex flex-col items-center justify-evenly bg-blue-900 text-white rounded-lg ">
+            <div className="font-bold text-lg">
             {questions && questions[currentQuestion].question}
             </div>
-            <div className="flex flex-col bg-red">
+            <div className="flex flex-col bg-red text-black">
             {questions &&
-              getRandom((questions[currentQuestion].answerOptions),  questions[currentQuestion].answerOptions.length).map((answerOption) => (
+              getRandom((questions[currentQuestion].answerOptions),  questions[currentQuestion].answerOptions.length).map((answerOption)=> (
                 <button
                 key={uuidv4()}
                 onClick={() =>
@@ -140,23 +128,12 @@ export default function Quiz() {
                   }
                   className={
                     answerOption.isCorrect && answerSelected && answerCorrect
-                    ? "border border-gray-400 rounded shadow my-4 bg-green-400 hover:bg-gray-200"
-                    : "border border-gray-400 rounded shadow my-4 bg-white hover:bg-gray-200"
+                    ? "border border-white rounded shadow my-4 p-4 bg-green-400 hover:bg-gray-200"
+                    : "border border-white rounded shadow my-4 p-4 bg-white hover:bg-gray-200"
                   }
                   >
                   {answerOption.answer}
-                  {/* <Modal show={show} onHide={handleShow}>
-              <Modal.Header closeButton>
-                <Modal.Title>Wrong response</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>The correct response is:</Modal.Body>
-              <Modal.Body>{answerOption.isCorrect && answerOption.answer}</Modal.Body>
-              <Modal.Footer>
-                <Button variant="primary" onClick={handleClose}>
-                  Got it👍
-                </Button>
-              </Modal.Footer>
-            </Modal> */}
+                
                   </button>
                   
                   ))}
--- insurance-frontend/tailwind.config.js
+++ insurance-frontend/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   plugins: [],
   theme: {
     extend: {
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
       fontFamily: {},
       backgroundImage: {},
       keyframes: {},
